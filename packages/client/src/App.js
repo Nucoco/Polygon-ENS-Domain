5acdf6d174edf2af35a5061afdbd6667a33229f2
Nucoco <yuuta11511997@gmail.com>
2024-04-27 03:03:11 +0000

    wallet接続機能

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,17 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
-// 定数
 const TWITTER_HANDLE = 'UNCHAIN_tech';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState('');
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert('Get MetaMask -> https://metamask.io/');
+        return;
+      }
+      const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log('Make sure you have MetaMask!');
+      return;
+    } else {
+      console.log('We have the ethereum object', ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+     if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log('Found an authorized account:', account);
+      setCurrentAccount(account);
+    } else {
+      console.log('No authorized account found');
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <div className="connect-wallet-container">
+      <img
+        src="https://media.giphy.com/media/3ohhwytHcusSCXXOUg/giphy.gif"
+        alt="Ninja gif"
+      />
+      <button
+        onClick={connectWallet}
+        className="cta-button connect-wallet-button"
+      >
+        Connect Wallet
+      </button>
+    </div>
+  );
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
     <div className="App">
       <div className="container">
-
         <div className="header-container">
           <header>
             <div className="left">
@@ -21,6 +74,8 @@ const App = () => {
           </header>
         </div>
 
+        {!currentAccount && renderNotConnectedContainer()}
+
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
@@ -28,11 +83,13 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built with @${TWITTER_HANDLE}`}</a>
+          >
+            {`built with @${TWITTER_HANDLE}`}
+          </a>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
