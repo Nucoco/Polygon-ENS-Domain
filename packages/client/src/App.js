a9db1151dc854ebd5b135b20708fbcd686fe65a9
Nucoco <yuuta11511997@gmail.com>
2024-04-27 03:47:02 +0000

    コントラクト呼び出し機能

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import contractAbi from './utils/contractABI.json';
 
 const TWITTER_HANDLE = 'UNCHAIN_tech';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const CONTRACT_ADDRESS = '0x0D79496635c7A951dcA9DA18c6fB2a8cFD7899C9';
+const tld = '.ninja';
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState('');
+  const [domain, setDomain] = useState('');
+  const [record, setRecord] = useState('');
 
   const connectWallet = async () => {
     try {
@@ -43,6 +49,60 @@ const App = () => {
     }
   };
 
+  const mintDomain = async () => {
+    if (!domain) {
+      return;
+    }
+    if (domain.length < 3) {
+      alert('Domain must be at least 3 characters long');
+      return;
+    }
+
+    const price =
+      domain.length === 3
+        ? '0.0000005'
+        : domain.length === 4
+          ? '0.0000003'
+          : '0.0000001';
+    console.log('Minting domain', domain, 'with price', price);
+
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        // connect to contract
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+
+        console.log('Going to pop wallet now to pay gas...');
+
+        // ドメイン登録
+        let tx = await contract.register(domain, {value: ethers.utils.parseEther(price)});
+
+        // tx実行待ち=ブロック取り込み待ち
+        const receipt = await tx.wait();
+
+        // トランザクションが問題なく実行されたか確認する
+        if (receipt.status === 1) {
+          console.log('Domain minted! https://mumbai.polygonscan.com/tx/' + tx.hash);
+
+          // ドメインにコンテンツを紐づけ
+          tx = await contract.setRecord(domain, record);
+          await tx.wait();
+
+          console.log('Record set! https://mumbai.polygonscan.com/tx/' + tx.hash);
+
+          setRecord('');
+          setDomain('');
+        } else {
+          alert('Transaction failed! Please try again');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const renderNotConnectedContainer = () => (
     <div className="connect-wallet-container">
       <img
@@ -57,6 +117,38 @@ const App = () => {
       </button>
     </div>
   );
+
+  const renderInputForm = () => {
+    return (
+      <div className="form-container">
+        <div className="first-row">
+          <input
+            type="text"
+            value={domain}
+            placeholder="domain"
+            onChange={(e) => setDomain(e.target.value)}
+          />
+          <p className="tld"> {tld} </p>
+        </div>
+
+        <input
+          type="text"
+          value={record}
+          placeholder="whats ur ninja power"
+          onChange={(e) => setRecord(e.target.value)}
+        />
+
+        <div className="button-container">
+          <button
+            className="cta-button mint-button"
+            onClick={mintDomain}
+          >
+            Mint
+          </button>
+        </div>
+      </div>
+    );
+  };
 
   useEffect(() => {
     checkIfWalletIsConnected();
@@ -75,6 +167,7 @@ const App = () => {
         </div>
 
         {!currentAccount && renderNotConnectedContainer()}
+        {currentAccount && renderInputForm()}
 
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
